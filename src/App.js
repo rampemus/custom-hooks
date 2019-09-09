ee026ca28c40814e38c32e8921f3eca37396e51c
rampemus <pasi.toivanen@live.com>
2019-09-09 16:23:19 +0300

    Custom hooks added

--- src/App.js
+++ src/App.js
@@ -1,75 +1,130 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-
 const useField = (type) => {
-  const [value, setValue] = useState('')
 
-  const onChange = (event) => {
-    setValue(event.target.value)
-  }
+    const [value, setValue] = useState('')
 
-  return {
-    type,
-    value,
-    onChange
-  }
+    const onChange = (event) => {
+        setValue(event.target.value)
+    }
+
+    const reset = () => {
+        setValue('')
+    }
+
+    return {
+        type,
+        value,
+        onChange,
+        reset
+    }
 }
 
 const useResource = (baseUrl) => {
-  const [resources, setResources] = useState([])
 
-  // ...
+    const [resources, setResources] = useState([])
 
-  const create = (resource) => {
-    // ...
-  }
+    const initAll = () => {
+        axios.get(baseUrl)
+            .then(response => {
+                setResources(response.data)
+                console.log(response.data)
+            })
+    }
 
-  const service = {
-    create
-  }
+    const create = (resource) => {
 
-  return [
-    resources, service
-  ]
+        const config = {
+            headers: { Authorization: 'token' }
+        }
+
+        const oldState = resources
+        const tempResource = {...resource, id:resources.length + 1}
+
+        setResources(oldState.concat(tempResource))
+        axios.post(baseUrl, resource, config)
+            .then(response => {
+                console.log('got response:', response.data)
+                console.log('should be same as', tempResource)
+            }).catch(error => {
+                console.log('error: ', error.message)
+                setResources(oldState)
+            })
+    }
+
+    const service = {
+        create,
+        initAll
+    }
+
+    return [
+        resources, service
+    ]
 }
 
 const App = () => {
-  const content = useField('text')
-  const name = useField('text')
-  const number = useField('text')
+    const content = useField('text')
+    const name = useField('text')
+    const number = useField('text')
 
-  const [notes, noteService] = useResource('http://localhost:3005/notes')
-  const [persons, personService] = useResource('http://localhost:3005/persons')
+    const [noData, setNoData] = useState(true)
+    const [notes, noteService] = useResource('http://localhost:3005/notes')
+    const [persons, personService] = useResource('http://localhost:3005/persons')
 
-  const handleNoteSubmit = (event) => {
-    event.preventDefault()
-    noteService.create({ content: content.value })
-  }
- 
-  const handlePersonSubmit = (event) => {
-    event.preventDefault()
-    personService.create({ name: name.value, number: number.value})
-  }
+    useEffect(() => {
+        if ( noData ) {
+            setNoData(false)
+            noteService.initAll()
+            personService.initAll()
+        }
+    },[noData, noteService, personService])
+    //above because if you write simply:
+    //useEffect(() => {
+    //    noteService.initAll()
+    //    personService.initAll()
+    //},[])
+    //it'll give you either infinite loop or warning
 
-  return (
-    <div>
-      <h2>notes</h2>
-      <form onSubmit={handleNoteSubmit}>
-        <input {...content} />
-        <button>create</button>
-      </form>
-      {notes.map(n => <p key={n.id}>{n.content}</p>)}
+    const handleNoteSubmit = (event) => {
+        event.preventDefault()
+        noteService.create({ content: content.value })
+        content.reset()
+    }
 
-      <h2>persons</h2>
-      <form onSubmit={handlePersonSubmit}>
-        name <input {...name} /> <br/>
-        number <input {...number} />
-        <button>create</button>
-      </form>
-      {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
-    </div>
-  )
+    const handlePersonSubmit = (event) => {
+        event.preventDefault()
+        personService.create({ name: name.value, number: number.value})
+        name.reset()
+        number.reset()
+    }
+
+    const previewArray = (array) => {
+        if ( array && array.length > 0 ) {
+            return array.map(n => <p key={n.id}>{n.content}</p>)
+        } else {
+            return <p>array is empty</p>
+        }
+    }
+
+    return (
+        <div>
+            <h2>notes</h2>
+            <form onSubmit={handleNoteSubmit}>
+                <input {...content} reset='null'/>
+                <button>create</button>
+            </form>
+            {notes.map(n => <p key={n.id}>{n.content}</p>)}
+
+            <h2>persons</h2>
+            <form onSubmit={handlePersonSubmit}>
+                name <input {...name} reset='null'/> <br/>
+                number <input {...number} reset='null'/>
+                <button>create</button>
+            </form>
+            {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+        </div>
+    )
 }
 
 export default App
